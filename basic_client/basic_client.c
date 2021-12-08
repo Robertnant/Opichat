e59aa0318bad908d61b0bbd3d3214b28707158a0
Clarel SALASSA <clarel.salassa@epita.fr>
2021-12-08 16:42:58 +0100

    basic_client: add basic_client.c

---
+++ basic_client/basic_client.c
@@ -0,0 +1,47 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <err.h>
+
+int create_and_connect(struct addrinfo *addrinfo)
+{
+    int sfd;
+    struct addrinfo *rp = NULL;
+    for (rp = addrinfo; rp != NULL; rp = rp->ai_next)
+    {
+        sfd = socket(rp->ai_familly, rp->ai_socktype, rp->ai_protocol);
+        if (sfd == -1)
+            continue;
+        if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+            break;
+        close(sfd);
+    }
+    if (rp == NULL)
+    {
+        err(1, "basic_client: connection failed");
+    }
+    freeaddrinfo(addrinfo);i
+    return sfd;
+}
+
+int prepare_socket(const char *ip, const char *port)
+{
+    struct addrinfo *addrinfo;
+    struct addrinfo hints;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    int value = getaddrinfo(ip, port, &hints, &addrinfo);
+    if (value != 0)
+        err(value, "basic_client: cannot get addresses");
+
+    return create_and_connect(addrinfo);
+}
+
+void communicate(int server_socket)
+{
+
+}
+
