c42a125d7ceb2e5f4bf0eb58f1eace157aa3c6bb
Clarel SALASSA <clarel.salassa@epita.fr>
2021-12-09 13:00:48 +0100

    basic_client: Modify Makefile and implement communicate function.

--- basic_client/basic_client.c
+++ basic_client/basic_client.c
@@ -1,7 +1,10 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
 #include <err.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "basic_client.h"
 
 int create_and_connect(struct addrinfo *addrinfo)
 {
@@ -9,7 +12,7 @@ int create_and_connect(struct addrinfo *addrinfo)
     struct addrinfo *rp = NULL;
     for (rp = addrinfo; rp != NULL; rp = rp->ai_next)
     {
-        sfd = socket(rp->ai_familly, rp->ai_socktype, rp->ai_protocol);
+        sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
         if (sfd == -1)
             continue;
         if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
@@ -20,7 +23,7 @@ int create_and_connect(struct addrinfo *addrinfo)
     {
         err(1, "basic_client: connection failed");
     }
-    freeaddrinfo(addrinfo);i
+    freeaddrinfo(addrinfo);
     return sfd;
 }
 
@@ -40,8 +43,54 @@ int prepare_socket(const char *ip, const char *port)
     return create_and_connect(addrinfo);
 }
 
+// Handle long messages by sending in file descriptor
+void resend(const char *buff, size_t len, int fd)
+{
+    size_t l = 0;
+    while (l < len)
+    {
+        ssize_t res = send(fd, buff + l, len - l, 0);
+        if (res == -1)
+        {
+            err(1, "basic_client: failed to send data");
+        }
+        l += res;
+    }
+}
+
+// Reads response from server and prints to sdout
+void print_response(int fd)
+{
+    char receive[DEFAULT_BUFFER_SIZE];
+    ssize_t n;
+    printf("Server answered with: ");
+    while ((n = recv(fd, receive, DEFAULT_BUFFER_SIZE, 0) != 0))
+    {
+        if (n == -1)
+            err(1, "basic_client: failed to receive response");
+
+        resend(receive, n, 1);
+    }
+}
+
+
 void communicate(int server_socket)
 {
+    ssize_t res;
+    char *lineptr = NULL;
+    size_t n = 0;
+    while (fprintf(stderr, "Enter your message:\n") &&
+            (res = getline(&lineptr, &n, stdin)) != -1)
+    {
+        resend(lineptr, res, server_socket);
+        print_response(server_socket);
 
+        //free memory and reset n
+        /*free(lineptr);
+        lineptr = NULL;
+        n = 0;*/
+    }
+    if (lineptr != NULL)
+        free(lineptr);
 }
 
