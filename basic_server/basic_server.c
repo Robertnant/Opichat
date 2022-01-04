6f6800b1131b122297dbfd53940a9ed9fde7b8b2
Clarel SALASSA <clarel.salassa@epita.fr>
2022-01-04 20:20:46 +0100

    basic_server: add Makefile basic_server.c basic_server.h

---
+++ basic_server/basic_server.c
@@ -0,0 +1,23 @@
+
+#include "basic.server.h"
+
+
+int create_and_bind(struct addrinfo *addrinfo)
+{
+
+}
+
+int prepare_socket(const char *ip, const char *port)
+{
+
+}
+
+int accept_client(int socket)
+{
+
+}
+
+void communicate(int client_socket)
+{
+
+}
