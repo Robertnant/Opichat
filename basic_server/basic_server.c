2ae43d397fa74480e01b439df5bc2c8209ca44e8
Robert Nantchouang Nantchouang <robert.nantchouang-nantchouang@epita.fr>
2022-01-06 18:12:08 +0100

    basic_server: move position of client disconnect prompt

--- basic_server/basic_server.c
+++ basic_server/basic_server.c
@@ -94,7 +94,6 @@ void communicate(int client_socket)
         // Move to next client when nothing is received.
         if (n == 0)
         {
-            write(1, "Client disconnected", sizeof("Client disconnected"));
             break;
         }
 
@@ -114,12 +113,14 @@ void communicate(int client_socket)
                         receive + l_send, n - l_send, MSG_NOSIGNAL);
 
                 if (errno == EPIPE)
+                {
+                    write(1, "Client disconnected",
+                            sizeof("Client disconnected"));
                     break;
+                }
 
-                /*
                 if (res == -1)
                     err(1, "failed to send data back to client");
-                */
 
                 l_send += res;
             }
@@ -128,12 +129,10 @@ void communicate(int client_socket)
             {
                 res = write(1, receive + l_write, n - l_write);
 
-                /*
                 if (res == -1)
                 {
                     err(1, "failed to write data to stdout");
                 }
-                */
 
                 l_write += res;
             }
